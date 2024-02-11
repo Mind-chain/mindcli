12dfe657f27ac6622ad01fadd3400b55e0617bdf
nnlgsakib <nlgarts@outlook.com>
2024-02-11 23:11:05 +0600

    init

---
+++ src/config/contracts.js
@@ -0,0 +1,8 @@
+const contracts = {
+    staking : "0x0000000000000000000000000000000000001001"
+    //will add more contracts soon
+}
+
+module.exports = {
+    contracts
+}
---
+++ src/config/providers.js
@@ -0,0 +1,15 @@
+const mainnet_rpc = {
+    http_rpc : "https://seednode.mindchain.info",
+    ws_rpc : "wss://seednode.mindchain.info/ws"
+};
+const testnet_rpc = {
+    http_rpc : "https://testnet-msc.mindchain.info",
+    ws_rpc : "wss://testnet-msc.mindchain.info/ws"
+};
+const explorer_api_base = "https://mainnet.mindscan.info/api/v2";
+
+module.exports = {
+    mainnet_rpc,
+    testnet_rpc,
+    explorer_api_base
+};
