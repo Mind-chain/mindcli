56ae6af0e8826108cfdfad63468941b522377864
nnlgsakib <nlgarts@outlook.com>
2024-02-14 13:21:49 +0600

    fixed som,e bugs and added faster response

--- src/commands/staking/showValidators.js
+++ src/commands/staking/showValidators.js
@@ -1,53 +1,68 @@
 // showValidators.js
 
-const { ethers } = require('ethers');
-const { stakingABI } = require('../../abi/stakingABI');
-const { urls, contracts } = require('../../config/index');
-const { erc20ABI } = require('../../abi/erc20');
+const { ethers } = require('ethers')
+const { stakingABI } = require('../../abi/stakingABI')
+const { urls, contracts } = require('../../config/index')
+const { erc20ABI } = require('../../abi/erc20')
 
 async function showValidators() {
   try {
-    console.log('Connecting to MIND network...');
+    console.log('Connecting to MIND network...')
 
     const provider = new ethers.providers.JsonRpcProvider(
-      urls.mainnet_rpc.http_rpc
-    );
+      urls.mainnet_rpc.http_rpc,
+    )
 
-    console.log('Connected to MIND network successfully.');
+    console.log('Connected to MIND network successfully.')
 
     // Load the contract
     const contract = new ethers.Contract(
       contracts.contracts.staking,
       stakingABI,
-      provider
-    );
+      provider,
+    )
     const rewardContract = new ethers.Contract(
       contracts.contracts.pmind,
       erc20ABI,
-      provider
-    );
+      provider,
+    )
 
-    console.log('Contract loaded successfully.');
+    console.log('Contract loaded successfully.')
 
-    console.log('Fetching validators...');
+    console.log('Fetching validators...')
 
-    // Fetch all validators
-    const validators = await contract.validators();
-    const totalStake = await contract.stakedAmount();
+    // Fetch all validators and their stakes
+    const [validators, totalStake] = await Promise.all([
+      contract.validators(),
+      contract.stakedAmount(),
+    ])
+
     console.log(
       'Total Mind Staked by Validators : ',
-      ethers.utils.formatEther(totalStake) + ' MIND'
-    );
+      ethers.utils.formatEther(totalStake) + ' MIND',
+    )
 
-    console.log('Validators:');
-    for (let index = 0; index < validators.length; index++) {
-      const validator = validators[index];
-      const rewardHolding = await rewardContract.balanceOf(validator);
-      console.log(`${index + 1}. ${validator} - Total PMIND received: ${ethers.utils.formatEther(rewardHolding)}`);
-    }
+    console.log('Validators:')
+
+    // Prepare promises for fetching rewards
+    const rewardPromises = validators.map(async (validator, index) => {
+      const rewardHolding = await rewardContract.balanceOf(validator)
+      return ethers.utils.formatEther(rewardHolding)
+    })
+
+    // Await all reward promises simultaneously
+    const rewards = await Promise.all(rewardPromises)
+
+    // Display results
+    rewards.forEach((reward, index) => {
+      const validator = validators[index]
+      console.log(
+        `${index + 1}. ${validator} - Total PMIND received: ${reward}`,
+      )
+    })
   } catch (error) {
-    console.error('Error occurred while fetching validators:', error);
+    console.error('Error occurred while fetching validators:', error)
   }
 }
 
-module.exports = { showValidators };
+module.exports = { showValidators }
--- src/config/contracts.js
+++ src/config/contracts.js
@@ -1,6 +1,6 @@
 const contracts = {
   staking: '0x0000000000000000000000000000000000001001',
-  pmind: '0x75E218790B76654A5EdA1D0797B46cBC709136b0'
+  pmind: '0x75E218790B76654A5EdA1D0797B46cBC709136b0',
   //will add more contracts soon
 }
 
